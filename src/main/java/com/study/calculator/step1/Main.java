926797257b998998ac7d82aa8f5a6eed55a5e859
hyun2ee2 <Hongkobi04@gmail.com>
2026-01-09 16:13:36 +0900

    feat: 입력 받는 값 순서 변경 첫번째 값 -> 두번째 값 -> 연산자 / 첫번째 값 -> 연산자 -> 두번째 값

--- src/main/java/com/study/calculator/step1/Main.java
+++ src/main/java/com/study/calculator/step1/Main.java
@@ -6,45 +6,50 @@ public class Main {
     public static void main(String[] args) {
         // 첫번째 값 스캐너
         Scanner numberScannerOne = new Scanner(System.in);
-        // 두번째 값 스캐너
-        Scanner numberScannerTwo = new Scanner(System.in);
+
         // 연산자 스캐너
         Scanner operator = new Scanner(System.in);
 
-        // 사용자로 부터 첫번째 값 입력 받기
-        System.out.print("첫번째 값을 입력해주세요 : ");
-        int numberOne = numberScannerOne.nextInt();
+        // 두번째 값 스캐너
+        Scanner numberScannerTwo = new Scanner(System.in);
 
-        // 사용자로 부터 두번째 값 입력 받기
-        System.out.print("두번째 값을 입력해주세요 : ");
-        int numberTwo = numberScannerTwo.nextInt();
 
-        // 사용자로 부터 연산자 입력 받기
-        System.out.print("연산자를 입력해주세요 : ");
-        String inputOperator = operator.nextLine();
 
-        // 조건문 (만약에 사용자로 부터 입력 받은 연산자가 "+" 이면 numberOne값과 numberTwo값을 더해서 출력)
-        if(inputOperator.equals("+")) {
-            int sum = numberOne + numberTwo;
 
-            System.out.println("결과 값 : " + sum);
-            // else-if (만약에 사용자로 부터 입력 받은 연산자가 "-" 이면 numberOne값과 numberTwo값을 빼서 출력)
-        } else if(inputOperator.equals("-")) {
-            int subtract = numberOne - numberTwo;
+            // 사용자로 부터 첫번째 값 입력 받기
+            System.out.print("첫번째 값을 입력해주세요 : ");
+            int numberOne = numberScannerOne.nextInt();
 
-            System.out.println("결과 값 : " + subtract);
-            // else-if (만약에 사용자로 부터 입력 받은 연산자가 "/" 이면 numberOne값과 numberTwo값을 나눈 값 출력)
-        } else if(inputOperator.equals("/")) {
-            // 몫과 나머지를 더해서 0.5를 뺀 값을 result에 저장
-            int divide = numberOne / numberTwo;
-            int remain = numberOne % numberTwo;
-            int result = divide + remain;
+            // 사용자로 부터 연산자 입력 받기
+            System.out.print("연산자를 입력해주세요 : ");
+            String inputOperator = operator.nextLine();
 
-            System.out.println("결과 값 : " + (result - 0.5));
-            //// else-if (만약에 사용자로 부터 입력 받은 연산자가 "*" 또는 "x" 이면 numberOne값과 numberTwo값을 나눈 값 출력)
-        } else if(inputOperator.equals("*") || inputOperator.equalsIgnoreCase("X")) {
-            int multiplication = numberOne * numberTwo;
-            System.out.println("결과 값 : " + multiplication);
-        }
+            // 사용자로 부터 두번째 값 입력 받기
+            System.out.print("두번째 값을 입력해주세요 : ");
+            int numberTwo = numberScannerTwo.nextInt();
+
+            // 조건문 (만약에 사용자로 부터 입력 받은 연산자가 "+" 이면 numberOne값과 numberTwo값을 더해서 출력)
+            if(inputOperator.equals("+")) {
+                int sum = numberOne + numberTwo;
+
+                System.out.println("결과 값 : " + sum);
+                // else-if (만약에 사용자로 부터 입력 받은 연산자가 "-" 이면 numberOne값과 numberTwo값을 빼서 출력)
+            } else if(inputOperator.equals("-")) {
+                int subtract = numberOne - numberTwo;
+
+                System.out.println("결과 값 : " + subtract);
+                // else-if (만약에 사용자로 부터 입력 받은 연산자가 "/" 이면 numberOne값과 numberTwo값을 나눈 값 출력)
+            } else if(inputOperator.equals("/")) {
+                // 몫과 나머지를 더해서 0.5를 뺀 값을 result에 저장
+                int divide = numberOne / numberTwo;
+                int remain = numberOne % numberTwo;
+                int result = divide + remain;
+
+                System.out.println("결과 값 : " + (result - 0.5));
+                //// else-if (만약에 사용자로 부터 입력 받은 연산자가 "*" 또는 "x" 이면 numberOne값과 numberTwo값을 나눈 값 출력)
+            } else if(inputOperator.equals("*") || inputOperator.equalsIgnoreCase("X")) {
+                int multiplication = numberOne * numberTwo;
+                System.out.println("결과 값 : " + multiplication);
+            }
     }
 }
