4eed2107cd8e27682ace7186e8d522d82b2c570a
hyun2ee2 <Hongkobi04@gmail.com>
2026-01-10 18:05:44 +0900

    feat: 사용자로부터 값과 연산자 입력 받는 기능 추가

--- src/main/java/com/study/calculator/step2/App.java
+++ src/main/java/com/study/calculator/step2/App.java
@@ -1,4 +1,27 @@
 package com.study.calculator.step2;
+import java.util.Scanner;
 
 public class App {
+        public static void app(String[] args) {
+                // 첫번째 값 스캐너
+                Scanner valueOneScanner = new Scanner(System.in);
+
+                // 연산자 스캐너
+                Scanner operatorScanner = new Scanner(System.in);
+
+                // 두번째 값 스캐너
+                Scanner valueTwoScanner = new Scanner(System.in);
+
+                // 사용자로 부터 첫번째 값 입력 받기
+                System.out.print("첫번째 값을 입력해주세요 : ");
+                int inputValueOne = valueOneScanner.nextInt();
+
+                // 사용자로 부터 연산자 입력 받기
+                System.out.print("첫번째 값을 입력해주세요 : ");
+                int inputOperator = operatorScanner.nextInt();
+
+                // 사용자로 부터 두번째 값 입력 받기
+                System.out.print("첫번째 값을 입력해주세요 : ");
+                int inputValueTwo = valueTwoScanner.nextInt();
+        }
 }
