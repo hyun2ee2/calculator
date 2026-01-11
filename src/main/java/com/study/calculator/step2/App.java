9bceaac2d9a23e162d80d8f9335fca482d4d244c
hyun2ee2 <Hongkobi04@gmail.com>
2026-01-11 21:38:52 +0900

    feat: 값을 저장하는 스캐너 추가, 값을 입력받는 기능 추가

--- src/main/java/com/study/calculator/step2/App.java
+++ src/main/java/com/study/calculator/step2/App.java
@@ -1,4 +1,24 @@
 package com.study.calculator.step2;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main() {
+        // 첫번째 값을 저장하는 스캐너
+        Scanner numberOneScanner = new Scanner(System.in);
+        // 연산자를 저장하는 스캐너
+        Scanner operatorScanner = new Scanner(System.in);
+        // 두번째 값을 저장하는 스캐너
+        Scanner numberTwoScanner = new Scanner(System.in);
+
+        System.out.println("첫번째 값을 입력해주세요 : ");
+        int numberOne = numberOneScanner.nextInt();
+
+        System.out.println("연산자를 입력해주세요 : ");
+        String operator = operatorScanner.nextLine();
+
+        System.out.println("두번째 값을 입력해주세요 : ");
+        int numberTwo = numberTwoScanner.nextInt();
+
+    }
 }
