ac1a6e7e5430341d33c1e6d284ff8f7258e810ff
hyun2ee2 <Hongkobi04@gmail.com>
2026-01-12 19:09:14 +0900

    feat: Calculator클래스 연산 기능 추가 및 App클래스 출력 기능 추가

--- src/main/java/com/study/calculator/step2/App.java
+++ src/main/java/com/study/calculator/step2/App.java
@@ -1,9 +1,12 @@
 package com.study.calculator.step2;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class App {
-    public static void main() {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
         // 첫번째 값을 저장하는 스캐너
         Scanner numberOneScanner = new Scanner(System.in);
         // 연산자를 저장하는 스캐너
@@ -11,14 +14,23 @@ public class App {
         // 두번째 값을 저장하는 스캐너
         Scanner numberTwoScanner = new Scanner(System.in);
 
-        System.out.println("첫번째 값을 입력해주세요 : ");
-        int numberOne = numberOneScanner.nextInt();
 
-        System.out.println("연산자를 입력해주세요 : ");
-        String operator = operatorScanner.nextLine();
+            System.out.print("첫번째 값을 입력해주세요 : ");
+            double numberOne = numberOneScanner.nextDouble();
 
-        System.out.println("두번째 값을 입력해주세요 : ");
-        int numberTwo = numberTwoScanner.nextInt();
+            System.out.print("연산자를 입력해주세요 : ");
+            String operator = operatorScanner.nextLine();
 
+            System.out.print("두번째 값을 입력해주세요 : ");
+            double numberTwo = numberTwoScanner.nextDouble();
+
+            double result = calculator.calculator(numberOne, numberTwo, operator);
+
+            // 만약에 calculator를 %로 계산 했을 때 나머지가 0일경우 calculator을 int형으로 변환해서 출력
+            if(calculator.result % 1 == 0) {
+                System.out.println("결과 값 : " + (int)calculator.result);
+            } else {
+                System.out.println("결과 값 : " + calculator.result);
+            }
     }
 }
--- src/main/java/com/study/calculator/step2/Calculator.java
+++ src/main/java/com/study/calculator/step2/Calculator.java
@@ -1,4 +1,35 @@
 package com.study.calculator.step2;
 
 public class Calculator {
+
+    double result = 0;
+    public double calculator(double numberOne, double numberTwo, String operator) {
+        switch (operator) {
+
+            // 만약에 operator가 "+"일 경우 result값에 numberOne + numberTwo를 진행 후 대입
+            case "+":
+                result = numberOne + numberTwo;
+                break;
+
+            // 만약에 operator가 "-"일 경우 result값에 numberOne - numberTwo를 진행 후 대입
+            case "-":
+                result = numberOne - numberTwo;
+                break;
+
+            // 만약에 operator가 "*"일 경우 result값에 numberOne * numberTwo를 진행 후 대입
+            case "*":
+                result = numberOne * numberTwo;
+                break;
+
+            // 만약에 operator가 "/"일 경우 result값에 numberOne / numberTwo를 진행 후 대입 (예외: 나누는 값이 0이될 경우 경고 메시지 출력)
+            case "/":
+                if(numberOne == 0) {
+                    System.out.println("0으로 나눌 수 없습니다 다시 시도해주세요.");
+                    return 0;
+                }
+                result = numberOne / numberTwo;
+                break;
+        }
+        return result;
+    }
 }
