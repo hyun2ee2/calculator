a653e19a6a0dbbac52cc914f6c5e7dcfa7a22d2e
hyun2ee2 <Hongkobi04@gmail.com>
2026-01-13 21:19:41 +0900

    feat: 스텝2 복습 끝!

--- src/main/java/com/study/calculator/step2/App.java
+++ src/main/java/com/study/calculator/step2/App.java
@@ -5,6 +5,64 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        // Calculator 객체 생성
+        Calculator calculator = new Calculator();
 
+        boolean complete = false;
+
+        while (true) {
+            // 숫자값 스캐너
+            Scanner numberScanner = new Scanner(System.in);
+            // 연산자 스캐너
+            Scanner operator = new Scanner(System.in);
+            // 종료여부 스캐너
+            Scanner exitWheather = new Scanner(System.in);
+
+            // 사용자로 부터 값 입력 받기
+            System.out.print("첫번째 값을 입력해주세요. : ");
+            double inputNumberOne = numberScanner.nextDouble();
+
+            System.out.print("연산자를 입력해주세요. : ");
+            String inputOperator = operator.nextLine();
+
+            System.out.print("두번째 값을 입력해주세요. : ");
+            double inputNumberTwo = numberScanner.nextDouble();
+
+            // 계산은 Calculator에서 받아옴
+            double result = calculator.result(inputNumberOne, inputNumberTwo, inputOperator);
+
+            if(calculator.result % 1 == 0) {
+                System.out.println("결과 값 : " + (int)result);
+            } else {
+                System.out.println("결과 값 : " + result);
+            }
+            // calcurationResult에 저장되어있는 이전 결과 값 출력 (소수는 그대로 출력하되 8.0과 같이 딱 맞아 떨어지면 .0을 제거하고 출력)
+            System.out.print("이전 계산 결과 : ");
+
+            for (double value : calculator.getCalculatorResult()) {
+                if (value % 1 == 0) {
+                    System.out.print((int) value + "ㅣ");
+                } else {
+                    System.out.print(value + "ㅣ");
+                }
+            }
+
+            System.out.println();
+            while (true) {
+                System.out.print("계산을 계속 진행하시겠습니까?(Y: 계속ㅣEXIT: 종료하기) : ");
+                String inputExitWheather = exitWheather.nextLine();
+                if (inputExitWheather.equalsIgnoreCase("Y")) {
+                    break;
+                } else if (inputExitWheather.equalsIgnoreCase("EXIT")) {
+                    complete = true;
+                    break;
+                } else {
+                    System.out.println("Y또는 EXIT를 입력해주시기 바랍니다.");
+                }
+            }
+            if(complete) {
+                break;
+            }
+        }
     }
 }
--- src/main/java/com/study/calculator/step2/Calculator.java
+++ src/main/java/com/study/calculator/step2/Calculator.java
@@ -1,6 +1,55 @@
 package com.study.calculator.step2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
 
+    private ArrayList<Double> calculationResult = new ArrayList<>();
 
+    double result = 0;
+    // 함수 생성
+    public double result(Double numberOne, Double numberTwo, String operator) {
+
+        switch (operator) {
+            case "+":
+                result = numberOne + numberTwo;
+                break;
+            case "-":
+                result = numberOne - numberTwo;
+                break;
+            case "*","x","X":
+                result = numberOne * numberTwo;
+                break;
+            case "/":
+                if(numberTwo == 0) {
+                    System.out.println("0으로 나눌 수 없습니다 다시 시도해주세요.");
+                    return 0;
+                }
+                result = numberOne / numberTwo;
+                break;
+            default:
+                System.out.println("연산자가 잘못입력 되었습니다.");
+                return 0;
+        }
+        // 계산 결과를 List에 저장
+        calculationResult.add(result);
+        return result;
+    }
+    // 결과 목록 Getter
+    public List<Double> getCalculatorResult() {
+        return calculationResult;
+    }
+
+    // 결과 목록 setter
+    public void setCalculationResult(List<Double> calculationResult) {
+        this.result = result;
+    }
+
+    // 가장 먼저 저장된 결과를 삭제하기
+    public void removeCalculationResult() {
+        if(!calculationResult.isEmpty()) {
+            calculationResult.remove(0);
+        }
+    }
 }
